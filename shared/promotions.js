f6e8ed59fafa2652a34890ba2a0521a4ab3fb571
Daniel Constante <daniel_constante86@hotmail.com>
2021-07-29 13:12:02 -0500

    Navigation Part 3

---
+++ shared/promotions.js
@@ -0,0 +1,9 @@
+export const PROMOTIONS = [
+    {
+        id: 0,
+        name: "Responsive User Interface",
+        image: "/assets/images/res.jpg",
+        featured: true,
+        description: "Responsive websites built for an optimal user experience that achieves your business goals."
+    }
+]
