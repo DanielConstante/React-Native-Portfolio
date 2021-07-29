73e8ced64d3c2de3f98f0e8370c3312f5c1de8a6
Daniel Constante <daniel_constante86@hotmail.com>
2021-07-29 12:12:16 -0500

    Components Part 1

---
+++ shared/campsites.js
@@ -0,0 +1,34 @@
+export const CAMPSITES = [
+    {
+        id: 0,
+        name: "Bootstrap Portfolio",
+        image: "/assets/images/bootstrap.png",
+        elevation: 1233,
+        featured: false,
+        description: "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS- and JavaScript-based design templates for typography, forms, buttons, navigation, and other interface components."
+    },
+    {
+        id: 1,
+        name:"React Portfolio ",
+        image: "/assets/images/react.png",
+        elevation: 877,
+        featured: false,
+        description: "React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications. "
+    },
+    {
+        id: 2,
+        name:"Bootstrap Campsites",
+        image: "/assets/images/bootcamp.png",
+        elevation: 2901,
+        featured: false,
+        description: "this site was created with Bootstap framework and responsive design that makes use of flexible layouts, flexible images and cascading style sheet media queries. The goal of responsive design is to build web pages that detect the visitor's screen size and orientation and change the layout accordingly."
+    },
+    {
+        id: 3,
+        name:"Front end development",
+        image: "/assets/images/dev.png",
+        elevation: 42,
+        featured: true,
+        description: "Markup and web languages such as HTML, CSS and JavaScript, specialized web editing software, image editing, accessibility, cross-browser issues and search engine optimisation."
+    }
+];
